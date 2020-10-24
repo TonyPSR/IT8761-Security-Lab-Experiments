2fa94fc7991f927d2c43fcc301c29102e8a095b6
tony_psr <s.tonypsr@gmail.com>
2020-10-24 12:22:38 +0530

    Added RailFenceCipher

---
+++ src/com/company/tonypsr/CaesarCipher.java
@@ -0,0 +1,57 @@
+package com.company.tonypsr;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CaesarCipher {
+
+    public static char[] encrypt(char[] plainText, int n){
+        char[] cipherText = new char[plainText.length];
+        for(int i=0; i<plainText.length; i++){
+            int offset = Character.isUpperCase(plainText[i])?65:97;
+            if(plainText[i] != ' ') {
+                cipherText[i] = (char) ((plainText[i] + n - offset) % 26 + offset);
+            } else {
+                cipherText[i] = ' ';
+            }
+        }
+
+        return cipherText;
+    }
+
+    public static char[] decrypt(char[] cipherText, int n){
+        char[] plainText = new char[cipherText.length];
+        for(int i=0; i<cipherText.length; i++){
+            int offset = Character.isUpperCase(cipherText[i])?65:97;
+            if(cipherText[i] != ' ') {
+                plainText[i] = (char) ((cipherText[i] + 26 - n - offset) % 26 + offset);
+            } else {
+                plainText[i] = ' ';
+            }
+        }
+
+        return plainText;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[] message;
+        int n;
+
+        //creating char array from string input
+        System.out.println("Enter the message: ");
+        message = sc.nextLine().toCharArray();
+        System.out.println("Enter the value of n: ");
+        n = sc.nextInt();
+
+        //encryption - input plain text
+        String cipherText = String.valueOf(encrypt(message, n));
+        System.out.println("Encrypted Text: " + cipherText);
+
+        //decryption - input encrypted cipher text
+        String plainText = String.valueOf(decrypt(cipherText.toCharArray(), n));
+        System.out.println("Decrypted Text: " + plainText);
+
+    }
+}
---
+++ src/com/company/tonypsr/RailFenceCipher.java
@@ -0,0 +1,117 @@
+package com.company.tonypsr;
+
+import java.util.Arrays;
+
+public class RailFenceCipher {
+    public static String encrypt(char[] plainText, int n){
+        StringBuilder cipherText = new StringBuilder();
+
+        char[][] rails = new char[n][plainText.length];
+
+        for(char[] rail: rails){
+            Arrays.fill(rail, '#');
+        }
+
+        int row = 0;
+        int col = 0;
+        int rowOffset = -1;
+
+
+        //Creating rails table
+        for (char letter : plainText) {
+            if (row == n - 1 || row == 0) {
+                rowOffset = -rowOffset;
+            }
+            rails[row][col] = letter;
+            row += rowOffset;
+            col += 1;
+        }
+
+        System.out.println("Encryption Rails:");
+        for(int i=0; i<rails.length; i++){
+            for(int j=0; j<rails[0].length; j++){
+                System.out.print(rails[i][j] + " ");
+                if(rails[i][j]!='#')
+                    cipherText.append(rails[i][j]);
+            }
+            System.out.println();
+        }
+
+
+        return cipherText.toString();
+    }
+
+    public static String decrypt(char[] cipherText, int n){
+        StringBuilder plainText = new StringBuilder();
+
+        char[][] rails = new char[n][cipherText.length];
+
+        for(char[] rail: rails){
+            Arrays.fill(rail, '#');
+        }
+
+        int row = 0;
+        int col = 0;
+        int rowOffset = -1;
+
+        //Creating rails table
+        for (int i=0; i<cipherText.length; i++) {
+            if (row == n - 1 || row == 0) {
+                rowOffset = -rowOffset;
+            }
+            rails[row][col] = '_';
+            row += rowOffset;
+            col += 1;
+        }
+
+        int x = 0;
+        for(int i=0; i<rails.length; i++){
+            for(int j=0; j<rails[0].length; j++) {
+                if (rails[i][j] == '_') {
+                    rails[i][j] = cipherText[x];
+                    x++;
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("Decryption Rails:");
+        for(int i=0; i<rails.length; i++){
+            for(int j=0; j<rails[0].length; j++){
+                System.out.print(rails[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        row = 0;
+        col = 0;
+        rowOffset = -1;
+
+        //Creating rails table
+        for (int i=0; i<cipherText.length; i++) {
+            if (row == n - 1 || row == 0) {
+                rowOffset = -rowOffset;
+            }
+            plainText.append(rails[row][col]);
+            row += rowOffset;
+            col += 1;
+        }
+
+        return plainText.toString();
+    }
+
+    public static void main(String[] args) {
+        char[] message = "ABCDEFGHIJKLMNO".toCharArray();
+        int n = 4;
+
+        String cipherText = encrypt(message, n);
+        System.out.println("Cipher Text: " + cipherText);
+
+        System.out.println();
+
+        String plainText = decrypt(cipherText.toCharArray(), n);
+        System.out.println("Plain Text: " + plainText);
+
+
+    }
+}
