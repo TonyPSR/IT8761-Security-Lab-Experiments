77a7101aee55a23f608fb038b06ce6ce3abb5fd6
tony_psr <s.tonypsr@gmail.com>
2020-10-29 11:11:06 +0530

    Implemented HillCipher encryption and decryption

--- src/com/company/tonypsr/substitution_and_transposition_ciphers/HillCipher.java
+++ src/com/company/tonypsr/substitution_and_transposition_ciphers/HillCipher.java
@@ -1,14 +1,17 @@
 package com.company.tonypsr.substitution_and_transposition_ciphers;
 
+import javax.swing.plaf.metal.MetalTheme;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class HillCipher {
 
-    public static ArrayList<String> splitMessage(String message){
+    public static ArrayList<String> splitMessage(String message, int splitBy){
         ArrayList<String> splittedMessage = new ArrayList<>();
 
-        for(int i=0; i<message.length()-1; i+=2){
-            splittedMessage.add(message.charAt(i) + "" + message.charAt(i+1));
+        for(int i=0; i<message.length()-splitBy+1; i+=splitBy){
+            splittedMessage.add(message.substring(i, i+splitBy));
         }
 
         return splittedMessage;
@@ -42,33 +45,151 @@ public class HillCipher {
     public static String encrypt(String plainText, int[][] key){
         StringBuilder cipherText = new StringBuilder();
 
-        ArrayList<String> messageSplit = splitMessage(plainText);
+        ArrayList<String> messageSplit = splitMessage(plainText, key.length);
+
 
         for(String letterPair: messageSplit){
-            int[][] c = multiply(key, stringToColumnVector(letterPair));
+            int[][] cipherBlock = multiply(key, stringToColumnVector(letterPair));
 
-            for(int i=0; i<c.length; i++){
-                cipherText.append((char)(c[i][0]%26+'a'));
+            for(int i=0; i<cipherBlock.length; i++){
+                cipherText.append((char)(cipherBlock[i][0]%26+'a'));
             }
         }
-
-
 
         return cipherText.toString();
     }
 
+
+    public static String decrypt(String cipherText, int[][] key){
+        StringBuilder plainText = new StringBuilder();
+
+        ArrayList<String> messageSplit = splitMessage(cipherText, key.length);
+
+        for(String letterPair: messageSplit){
+            int[][] adjMatrix = adj(key);
+            int determinant = determinant(key, key.length);
+
+            int[][] c = multiply(adjMatrix, stringToColumnVector(letterPair));
+
+
+            //finding suitable k such that
+            // (det*k)mod26 === 1
+            int k=1;
+            while((determinant*k)%26 != 1){
+                k++;
+            }
+
+            for(int i=0; i<c.length; i++){
+                c[i][0] *= k;
+                plainText.append((char)(c[i][0]%26+'a'));
+            }
+
+        }
+
+        return plainText.toString();
+    }
+
+
+    static void getCofactor(int[][] A, int[][] temp, int p, int q, int n) {
+
+        int i = 0, j = 0;
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+
+                if (row != p && col != q) {
+                    temp[i][j++] = A[row][col];
+
+                    if (j == n - 1)
+                    {
+                        j = 0;
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+
+    static int determinant(int[][] A, int n)
+    {
+        int result = 0;
+
+        if (n == 1)
+            return A[0][0];
+
+        int[][] temp = new int[A.length][A.length];
+
+        int sign = 1;
+
+        for (int f = 0; f < n; f++) {
+            getCofactor(A, temp, 0, f, n);
+            result += sign * A[0][f] * determinant(temp, n - 1);
+
+            sign = -sign;
+        }
+
+        return result;
+    }
+
+
+    static int[][] adj(int A[][])
+    {
+        int[][] adj = new int[A.length][A.length];
+        if (A.length == 1)
+        {
+            adj[0][0] = 1;
+            return adj;
+        }
+
+        int sign = 1;
+        int [][]temp = new int[A.length][A.length];
+
+        for (int i = 0; i < A.length; i++)
+        {
+            for (int j = 0; j < A.length; j++)
+            {
+                getCofactor(A, temp, i, j, A.length);
+
+                sign = ((i + j) % 2 == 0)? 1: -1;
+
+                adj[j][i] = (sign)*(determinant(temp, A.length-1));
+            }
+        }
+
+        return adj;
+    }
+
+    public static int[][] stringToIntArray(String message){
+        int n = (int) Math.sqrt(message.length());
+        int[][] key = new int[n][n];
+
+        int stringPtr = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                key[i][j] = message.charAt(stringPtr);
+                stringPtr+=1;
+            }
+        }
+
+        return key;
+    }
+
+
     public static void main(String[] args) {
-        int keyLenght = 2;
+        Scanner sc = new Scanner(System.in);
 
-        int[][] key = {
-                {3,3},
-                {2,5}
-        };
+        System.out.println("Enter the message: ");
+        String message = sc.next().toLowerCase();
 
-        String message = "helloo";
+        System.out.println("Enter the key: (length: 4, 9, etc...)");
+        String keyString = sc.next().toLowerCase();
 
-        System.out.println(encrypt(message, key));
+        int[][] key = stringToIntArray(keyString);
 
+        String cipherText = encrypt(message, key);
+        System.out.println("Cipher Text: " + cipherText);
+
+        String plainText = decrypt(cipherText, key);
+        System.out.println("Plain Text: " + plainText);
 
     }
 }
